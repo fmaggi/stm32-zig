fbcec90fba9734a1facab691c913dfd5d2edc237
Francisco <franciscomaggi1999@gmail.com>
2024-05-02 11:28:20 -0300

    updated to zig 0.12.0

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "An empty project that uses Zig.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     zig.url = "github:mitchellh/zig-overlay";
@@ -37,7 +37,7 @@
       in rec {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            zigpkgs.master
+            zigpkgs."0.12.0"
           ];
         };
 
